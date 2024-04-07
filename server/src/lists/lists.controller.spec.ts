1b2b976fdcce7ab06ea7f9ace02ecfe948430173
Parle648 <sashahoumi@gmail.com>
2024-04-07 09:22:53 +0200

    Create API tests for lists controller

--- server/src/lists/lists.controller.spec.ts
+++ server/src/lists/lists.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ListsController } from './lists.controller';
 import { ListsService } from './lists.service';
 import { DatabaseService } from 'src/database/database.service';
+import request from 'supertest';
 
 describe('ListsController', () => {
   let controller: ListsController;
@@ -18,4 +19,77 @@ describe('ListsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('GET all lists', async () => {
+    const lists = await request('http://localhost:3001').get('/lists')
+
+    expect(lists.status).toBe(200)
+  })
+  
+  it('GET all lists by board', async () => {
+    const lists = await request('http://localhost:3001').get('/lists?board_id=1')
+
+    expect(lists.status).toBe(200)
+  })
+
+  it('POST list', async () => {
+    const lists = await request('http://localhost:3001')
+    .post('/lists')
+    .send({
+      listData: {
+        list_name: 'List',
+        board_id: 1
+      },
+      action: {
+          activity_type: "createList",
+          from: "",
+          to: 'List',
+          list_name: 'List',
+          board_id: 1
+      }
+    })
+
+    expect(lists.status).toBe(201)
+  })
+
+  it('PATCH list', async () => {
+    const lists = await request('http://localhost:3001')
+    .patch('/lists/1')
+    .send({
+      listData: {
+        list_name: 'List',
+        board_id: 1
+      },
+      action: {
+          activity_type: "createList",
+          from: "",
+          to: 'List',
+          list_name: 'List',
+          board_id: 1
+      }
+    })
+
+    expect(lists.status).toBe(200)
+  })
+
+  it('DELETE list', async () => {
+    const existLists = await request('http://localhost:3001').get('/lists')
+    const lists = await request('http://localhost:3001')
+    .patch(`/lists/${existLists.body[0].id}`)
+    .send({
+      listData: {
+        list_name: 'List',
+        board_id: 1
+      },
+      action: {
+          activity_type: "createList",
+          from: "",
+          to: 'List',
+          list_name: 'List',
+          board_id: 1
+      }
+    })
+
+    expect(lists.status).toBe(200)
+  })
 });
